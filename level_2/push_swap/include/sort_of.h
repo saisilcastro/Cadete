f31d2a5ebd48f27cdd0e133427ef6811c44e9e48
saisilcastro <saisilcastro@gmail.com>
2023-09-20 21:56:19 -0300

    re-implementing push_swap

--- level_2/push_swap/include/sort_of.h
+++ level_2/push_swap/include/sort_of.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort_of.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mister-coder <mister-coder@student.42.f    +#+  +:+       +#+        */
+/*   By: lde-cast <lde-cast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 00:12:06 by mister-code       #+#    #+#             */
-/*   Updated: 2023/08/19 20:31:08 by mister-code      ###   ########.fr       */
+/*   Updated: 2023/09/20 20:04:31 by lde-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,21 @@
 # include <stack_of.h>
 # include <ft_printf.h>
 
+typedef struct s_cost	t_cost;
+struct s_cost{
+	int	a;
+	int	b;
+	int	index_a;
+	int	index_b;
+};
+
 typedef struct s_sort	t_sort;
 struct s_sort{
 	t_stack	*a;
 	t_stack	*b;
 	int		a_max;
 	int		b_max;
+	t_cost	cost;
 };
 
 extern void	sort_set(t_sort	*set);
@@ -36,6 +45,7 @@ extern void	sort_rr(t_sort *set, int i);
 extern void	sort_rra(t_sort *set, int i);
 extern void	sort_rrb(t_sort *set, int i);
 extern void	sort_rrr(t_sort *set, int i);
+extern int	sort_cheaper(t_sort *set);
 extern int	sort_resolve(t_sort *set);
 extern void	sort_a_apply(t_sort *set);
 extern void	sort_b_apply(t_sort *set);
--- level_2/push_swap/src/sort-resolve.c
+++ level_2/push_swap/src/sort-resolve.c
@@ -6,7 +6,7 @@
 /*   By: lde-cast <lde-cast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 18:48:38 by mister-code       #+#    #+#             */
-/*   Updated: 2023/09/11 17:20:49 by lde-cast         ###   ########.fr       */
+/*   Updated: 2023/09/20 17:09:51 by lde-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,15 +48,11 @@ void	down_half(t_sort *set, int i)
 	sort_push(set, 'b');
 }
 
-int	sort_resolve(t_sort *set)
+void	little_sort(t_sort *set)
 {
 	int	i;
 
-	if (is_sorted(set))
-		return (1);
-	if (!set || set->b_max > 0)
-		return (0);
-	while (set->a_max > 3 && is_sorted(set))
+	while (set->a_max > 3 && !is_sorted(set))
 	{
 		i = stack_pos(set->a, stack_min(set->a));
 		if (i > set->a_max / 2)
@@ -67,5 +63,21 @@ int	sort_resolve(t_sort *set)
 	stack_sort_three(&set->a, 'a');
 	while (set->b_max)
 		sort_push(set, 'a');
+}
+
+int	sort_resolve(t_sort *set)
+{
+	if (is_sorted(set))
+		return (1);
+	if (!set || set->b_max > 0)
+		return (0);
+	if (set->a_max <= 5)
+		little_sort(set);
+	else
+	{
+		sort_push(set, 'b');
+		sort_push(set, 'b');
+		sort_cheaper(set);
+	}
 	return (1);
 }
