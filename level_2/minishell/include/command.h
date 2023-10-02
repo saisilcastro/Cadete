2641f3f2797687d27bde022c722613ea825bd372
mister-coder <saisilcastro@gmail.com>
2023-10-02 14:31:37 -0300

    minishell starte

--- level_2/minishell/include/command.h
+++ level_2/minishell/include/command.h
@@ -1,29 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   command.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mister-coder <mister-coder@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/29 18:24:46 by lde-cast          #+#    #+#             */
-/*   Updated: 2023/10/01 14:21:43 by mister-code      ###   ########.fr       */
+/*   Created: 2023/10/01 12:47:56 by mister-code       #+#    #+#             */
+/*   Updated: 2023/10/01 14:22:35 by mister-code      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef COMMAND_H
+# define COMMAND_H
 
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <command.h>
+#include <stdlib.h>
 
-typedef struct s_minishell	t_minishell;
-struct s_minishell{
-	t_command	*cmd;
+typedef struct s_flag	t_flag;
+struct s_flag{
+	char	*name;
+	t_flag	*prev;
+	t_flag	*next;
 };
 
-extern void	shell_set(t_minishell *set);
-extern void	shell_command(t_minishell *set);
-extern void	shell_pop(t_minishell *set);
+extern t_flag	*flag_push(char *name);
+extern void		flag_next_first(t_flag **head, t_flag *set);
+extern void		flag_next_last(t_flag **head, t_flag *set);
+extern void		flag_pop(t_flag *set);
+
+typedef struct s_command	t_command;
+struct s_command{
+	char	*run;
+	t_flag	*flag;
+};
 
 #endif
--- level_2/minishell/src/minishell.c
+++ level_2/minishell/src/minishell.c
@@ -3,14 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lde-cast <lde-cast@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mister-coder <mister-coder@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 18:25:22 by lde-cast          #+#    #+#             */
-/*   Updated: 2023/09/29 21:55:21 by lde-cast         ###   ########.fr       */
+/*   Updated: 2023/10/01 14:22:03 by mister-code      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+void	shell_set(t_minishell *set)
+{
+	if (!set)
+		return ;
+	set->cmd = NULL;
+}
 
 void	shell_pop(t_minishell *set)
 {
